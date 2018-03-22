76eaaac0d91e7e7e4d5b469fe57ff5a590fe61a6
César Augusto <cezzaar@gmail.com>
2018-03-22 17:59:14 -0300

    wip

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Request;
+use App\Models\Pessoa;
+
+class Helper
+{
+    public static function registroPorTipoCadastro($tipo = 1)
+    {
+        $slug = str_slug($tipo);
+
+        $itens = session()->has($slug);
+
+        if(!$itens) {
+
+          $itens = Pessoa::where('tipo_cadastro_id', $tipo)->get();
+
+          session()->put($slug, $itens);
+
+        }
+
+        return session()->get($slug)->count();
+    }
+
+    public static function tiposCadastro()
+    {
+        $slug = 'tipo_cadastro';
+
+        $itens = session()->has($slug);
+
+        if(!$itens) {
+
+          $itens = TipoCadastro::all();
+
+          session()->put($slug, $itens);
+
+        }
+
+        return session()->get($slug)->count();
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,70 +14,181 @@
 @section('content')
 
     <div class="row">
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
 
-          <div class="info-box-content">
-            <span class="info-box-text">Propostas Compra</span>
-            <span class="info-box-number">{{ App\Helpers\PropostaHelper::getPropostasPorTipo() }}</span>
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Propostas Locação</span>
-            <span class="info-box-number">{{ App\Helpers\PropostaHelper::getPropostasPorTipo('Proposta Locacao') }}</span>
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Propostas Lançamentos</span>
-              <span class="info-box-number">{{ App\Helpers\PropostaHelper::getPropostasPorTipo('Proposta Lancamento') }}</span>
+      <div class="col-md-4">
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">Propostas de Compra</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-green">{{ App\Helpers\PropostaHelper::getPropostasPorTipo() }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
             </div>
           </div>
-        </div>
-
-      <!--
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Uploads</span>
-            <span class="info-box-number">13,648</span>
-          </div>
-
-        </div>
-
       </div>
 
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="info-box">
-          <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
-
-          <div class="info-box-content">
-            <span class="info-box-text">Likes</span>
-            <span class="info-box-number">93,139</span>
+      <div class="col-md-4">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Propostas de Locação</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-blue">{{ App\Helpers\PropostaHelper::getPropostasPorTipo('Proposta Locacao') }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
           </div>
-
-        </div>
-
       </div>
-    -->
+
+      <div class="col-md-4">
+          <div class="box box-danger">
+            <div class="box-header with-border">
+              <h3 class="box-title">Propostas de Lançamentos</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-red">{{ App\Helpers\PropostaHelper::getPropostasPorTipo('Proposta Lancamento') }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+
+      <div class="col-md-3">
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">Compradores PF</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-green">{{ App\Helpers\Helper::registroPorTipoCadastro(1) }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-3">
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">Compradores PJ</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-green">{{ App\Helpers\Helper::registroPorTipoCadastro(2) }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-3">
+          <div class="box box-warning">
+            <div class="box-header with-border">
+              <h3 class="box-title">Locatários PF</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-yellow">{{ App\Helpers\Helper::registroPorTipoCadastro(3) }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+
+      <div class="col-md-3">
+          <div class="box box-warning">
+            <div class="box-header with-border">
+              <h3 class="box-title">Locatários PJ</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-yellow">{{ App\Helpers\Helper::registroPorTipoCadastro(4) }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-3">
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Fiadores PF</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-blue">{{ App\Helpers\Helper::registroPorTipoCadastro(5) }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-3">
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Fiadores PJ</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-light-blue">{{ App\Helpers\Helper::registroPorTipoCadastro(6) }}</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-6">
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">Imóveis Cadastrados</h3>
+              <div class="box-tools pull-right">
+                <span data-toggle="tooltip" title="" class="badge bg-yellow" data-original-title="3 New Messages" aria-describedby="tooltip435290">3</span>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-6">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Imóveis Encomendados</h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Aparecerão aqui
+            </div>
+          </div>
+      </div>
+
     </div>
 
 @stop
