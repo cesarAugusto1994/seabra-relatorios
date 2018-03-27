c876ada3753ba07ce82e7fb32447a24754b7d50e
Cesar Augusto <cezzaar@gmail.com>
2018-03-26 23:50:28 -0300

    wip

--- database/migrations/2018_03_27_022040_create_solicitar_informacoes_table.php
+++ database/migrations/2018_03_27_022040_create_solicitar_informacoes_table.php
@@ -21,6 +21,12 @@ class CreateSolicitarInformacoesTable extends Migration
             $table->text('duvida')->nullable();
             $table->string('imovel')->nullable();
             $table->string('qtde_imoveis')->nullable();
+            $table->integer('imovel_1')->nullable();
+            $table->integer('imovel_2')->nullable();
+            $table->integer('imovel_3')->nullable();
+            $table->integer('imovel_4')->nullable();
+            $table->integer('imovel_5')->nullable();
+            $table->integer('imovel_6')->nullable();
             $table->string('formulario_origem')->nullable();
             $table->timestamps();
         });
